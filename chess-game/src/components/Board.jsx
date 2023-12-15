dfc5ed279ec0867fd9e51a0a8fa13adde4d15e60
Romulo <romulotg12@gmail.com>
2023-12-15 19:20:22 -0300

    feat: board added

---
+++ chess-game/src/components/Board.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+import { Chessboard }from "react-chessboard";
+import { Chess } from "chess.js";
+
+export default function Board() {
+    return (
+
+        <Chessboard/>
+            
+    );
+}
