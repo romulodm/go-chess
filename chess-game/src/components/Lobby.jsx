ae8d48358d769e593572586606522c669753727e
Romulo <romulotg12@gmail.com>
2023-12-15 19:24:35 -0300

    feat: connecting to the socket through the lobby

--- chess-game/src/components/Lobby.jsx
+++ chess-game/src/components/Lobby.jsx
@@ -3,10 +3,8 @@ import React, { useState, useEffect } from "react";
 import generateRandomGameCode from "../scripts/generateGameCode";
 import io from 'socket.io-client';
 
-import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import Slide from '@mui/material/Slide';
-import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 
@@ -25,116 +23,75 @@ const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
+  border: 1,
+  borderColor: "#EFEFEF",
+  borderRadius: "8px",
   transform: 'translate(-50%, -50%)',
   width: 350,
-  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
 };
 
-const baseURL = 'http://127.0.0.1:800/';
+var SOCKET_BASE_URL = "ws://localhost:8000"
 
 export default function Lobby(props) {
-
     // Exibir loading (o círculo que fica rodando):
     const [isLoading, setIsLoading] = useState(false);
     //
 
-    // Para fechar o modal:
-    const [openAlert, setOpen] = useState(true);
-    const handleClose = () => {
-        setOpen(false);
-        props.setOpenLobby(false);
+    // Código que será inserido pelo usuário:
+    const [gameCode, setGamecode] = useState("");
+    //
+
+    // Fazer uma solicitação para criar uma nova sala:
+    const handleNewGame = async (e) => {
+        e.preventDefault();
+
+        var codeToSend = generateRandomGameCode();
+
+        const socket = new WebSocket(`${SOCKET_BASE_URL}/create-room?id=${codeToSend}`)
+
+        socket.addEventListener("message", (event) => {
+            const messageFromServer = JSON.parse(event.data);
+            console.log(messageFromServer)
+            props.setCodeToShow(messageFromServer.message);
+        });
+
+        props.setWebSocket(socket);
     }
     //
 
-    // Mensagens de alerta que são mostradas na parte inferior:
-    const [showAlertMessage, setShowAlertMessage] = useState(false); // Exibir ou não;
-    const [alertMessage, setAlertMessage] = useState(""); // Mensagem;
-    const [typeAlertMessage, setTypeAlertMessage] = useState(''); // Tipo da mensagem: success, error, warning ou info;
-
-    const handleAlertMessage = (type, message) => {
-        setShowAlertMessage(true);
-        setAlertMessage(message);
-        setTypeAlertMessage(type);
-
-        setTimeout(() => { setShowAlertMessage(false) }, 7000); // Fechar a mensagem;
-    };
-    //
-
-    // Código que será inserido pelo usuário:
-    const [gamecode, setGamecode] = useState("");
-    //
-
-    const handleNewGame = (e) => {
-        e.preventDefault;
-
-        var codeGenerated = generateRandomGameCode()
-        
-        try {
-          setIsLoading(true);
-          const newSocket = io(`http://localhost:8000/join-room/${codeGenerated}`);
-          props.setWebSocket(newSocket);
-    
-          newSocket.on('connect', () => {
-            handleAlertMessage('success','Conectado ao servidor WebSocket');
-            setIsLoading(false);
-            // Lógica adicional, se necessário
-          });
-
-        } catch (error) {
-            handleAlertMessage('error','Erro ao conectar ao servidor WebSocket:');
-            console.log(error)
-          setIsLoading(false);
-        }
-      };
-    
-      const handleJoinGame =  (e) => {
+    // Fazer uma solicitação para entrar em uma sala existente:
+    const handleJoinGame = (e) => {
         e.preventDefault();
 
-        try {
-          setIsLoading(true);
-          const newSocket = io(`http://localhost:8000/join-room/${gamecode}`);
-          setSocket(newSocket);
-    
-          newSocket.on('connect', () => {
-            handleAlertMessage('success','Conectado ao servidor WebSocket');
-            setIsLoading(false);
-          });
+        const regexGameCode = /^[A-Z0-9]+$/;
 
-        } catch (error) {
-            handleAlertMessage('error','Erro ao conectar ao servidor WebSocket:');
-            console.log(error)
-            setIsLoading(false);
+        if (gameCode.length < 7 || !regexGameCode.test(gameCode)) {
+            props.handleAlertMessage("error", "Enter a valid code, please.");
         }
-      };
+
+        const socket = new WebSocket(`${SOCKET_BASE_URL}/join-room?id=${gameCode}`)
+
+        socket.onopen = function (event) {
+            console.log('Conexão estabelecida com sucesso!');
+        };
+
+        socket.onerror = function (error) {
+            props.handleAlertMessage("error", "")
+        };
+
+        socket.addEventListener("message", (event) => {
+            const messageFromServer = JSON.parse(event.data);
+            console.log(messageFromServer)
+            props.setCodeToShow(messageFromServer.message);
+        });
+
+        //props.setWebSocket(socket);
+    }
+    //
 
     return (
-    <Modal
-        open={openAlert}
-        onClose={handleClose}
-        style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            backdropFilter: 'blur(7px)',
-        }}
-        >
-        <>
 
-        <Snackbar
-            open={showAlertMessage}
-            severity="success"
-            TransitionComponent={SlideTransition}
-            anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "center"
-            }}>
-            <Alert  severity={typeAlertMessage} sx={{ width: '100%' }}>
-            {alertMessage}
-            </Alert>
-        </Snackbar>
-
-        <Box sx={style}>
+    <Box sx={style}>
 
         <div className="bg-white rounded-xl shadow-lg">
         
@@ -147,7 +104,7 @@ export default function Lobby(props) {
                             <div className="flex flex-col items-center bg-white text-gray-700 py-6 text-center">
                             
                                 <button  
-                                    onClick={(e) => handleNewGame}
+                                    onClick={handleNewGame}
                                     className="inline-flex w-40 hover:bg-blue-600 justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300"
                                 >
                                     <PlayCircleOutlineOutlinedIcon style={{marginRight: "5px"}}/>
@@ -165,12 +122,12 @@ export default function Lobby(props) {
                                     <input
                                         className="game-code-lobby"
                                         maxLength="7"
-                                        value={gamecode.toUpperCase()}
+                                        value={gameCode.toUpperCase()}
                                         onChange={(e) => setGamecode(e.target.value)}
                                     />
                                     </div>
                                     <button
-                                        onClick={(e) => handleJoinGame}
+                                        onClick={handleJoinGame}
                                         className="inline-flex ml-3 hover:bg-blue-600 bg-blue-700 justify-center items-center py-3 px-4 text-base font-medium text-center text-white rounded-lg bg-blue-700 focus:ring-4"
                                     >   
                                         <LoginOutlinedIcon style={{marginRight: "7px"}}/>
@@ -196,8 +153,7 @@ export default function Lobby(props) {
             </Box>
             )}
         </React.Fragment>
-        </Box>
-        </>      
-    </Modal>
+
+    </Box>
   )
 }
