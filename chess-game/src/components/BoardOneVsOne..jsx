985a4e7b29a92f8ed2dd013e3935e4b965967c0a
Romulo <romulotg12@gmail.com>
2023-12-17 01:16:17 -0300

    feat: a board that consumes the ChessApp class was added

---
+++ chess-game/src/components/BoardOneVsOne..jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Chessboard from "chessboardjsx";
+
+import ChessApp from "../chess/ChessApp.jsx";
+
+const boardStyle = {
+    borderRadius: "5px",
+    boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+};
+
+export default function BoardOneVsOne(props) {
+    const boardPerspective = () => {
+        if (props.userName) {
+          return props.userName == "Player 2" ? "black" : "white"
+        }
+        return "white"
+      }
+      
+    return (
+      <div className="flex justify-around items-center">
+        <ChessApp userName={props.userName} webSocket={props.webSocket}>
+          {({ position, onDrop }) => (
+            <Chessboard
+              id="ChessApp"
+              position={position}
+              calcWidth={({ screenWidth, screenHeight }) => Math.min(screenWidth, screenHeight) * 0.95}
+              onDrop={onDrop}
+              boardStyle={boardStyle}
+              lightSquareStyle={{ backgroundColor: "AliceBlue" }}
+              darkSquareStyle={{ backgroundColor: "#C3C3C3" }}
+
+              orientation={boardPerspective()}
+            />
+          )}
+        </ChessApp>
+      </div>
+    );
+}
