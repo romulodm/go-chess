5fe404b6245d233a67eba5abf3dcd673148e8f91
Romulo <romulotg12@gmail.com>
2024-09-23 22:08:18 -0300

    feat: adding Vercel's analytics tool to measure the number of people using the system (migrating from Render to Vercel)

--- chess-game/src/main.jsx
+++ chess-game/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from '@mui/system';
 import { theme } from './theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <>
+    <Analytics/>
+    
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </>
 )
